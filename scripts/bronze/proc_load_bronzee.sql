c51ab13f62089d408f4f3fe47d677f46a1b60df6
ergi <102044261+ergixara@users.noreply.github.com>
2026-02-11 01:14:58 +0100

    Update and rename ddl_bronze.sql to proc_load_bronzee.sql

--- scripts/bronze/proc_load_bronzee.sql
+++ scripts/bronze/proc_load_bronzee.sql
@@ -1,13 +1,21 @@
 /*
 ===============================================================================
-DDL Script: Create Bronze Tables
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
 ===============================================================================
 Script Purpose:
-    This script creates tables in the 'bronze' schema, dropping existing tables 
-    if they already exist.
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
 ===============================================================================
 */
-
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
 	DECLARE @start_time Datetime , @end_time Datetime , @batch_start_time Datetime , @batch_end_time Datetime
